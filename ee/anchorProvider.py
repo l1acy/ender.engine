71b38d52c0119c83267abcd1566da9e3766032cc
l1acy <101744830+l1acyy@users.noreply.github.com>
2024-05-29 22:32:54 +0300

    Upload ender.engine files

---
+++ ee/anchorProvider.py
@@ -0,0 +1,22 @@
+from pygame import Surface
+
+type crd = tuple[int, int]
+
+class AnchorProvider:
+    def c(x: int, y: int, screen: Surface) -> crd:
+        cx, cy = screen.get_size()
+
+        return (cx + x, cy + y)
+    
+    def lu(x: int, y: int, screen: Surface) -> crd:
+        return (x, y)
+    
+    def lc(x: int, y: int, screen: Surface) -> crd:
+        cy = screen.get_height() / 2
+
+        return (x, cy + y)
+    
+    def ld(x: int, y: int, screen: Surface) -> crd:
+        height = screen.get_height()
+
+        return (x, height - y)
---
+++ ee/route.py
@@ -0,0 +1,44 @@
+from dataclasses import dataclass
+
+from pygame import (
+    init as app_init,
+    display as display,
+    Surface,
+    RESIZABLE,
+    NOFRAME
+)
+
+from pygame.time import Clock
+
+@dataclass
+class RouterSize:
+    width: int = 500
+    height: int = 300
+
+class Router():
+    def __init__(
+        self,
+        title: str = "enger.engine",
+        size = RouterSize(),
+        resizable: bool = True,
+    ):
+        self.title = title
+        self.size = size
+
+        self.resizeable = resizable
+
+    def build(self) -> Surface:
+        app_init()
+        screen = display.set_mode((self.size.width, self.size.height), RESIZABLE)
+        display.set_caption(self.title)
+
+        return screen
+
+    def fps(self):
+        return Clock()
+
+    def update():
+        display.update()
+
+    def flip():
+        display.flip()
