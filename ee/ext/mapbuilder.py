c0a9a228cfa75e3cd50a57c19e166b43f1ebd237
l1acy <mantittap@gmail.com>
2024-07-16 15:44:06 +0300

    update map builder

--- ee/ext/mapbuilder.py
+++ ee/ext/mapbuilder.py
@@ -5,6 +5,8 @@ from ee import Router
 
 from pygame import image
 
+from pygame import transform
+
 
 class MapBuilder:
     """
@@ -18,7 +20,7 @@ class MapBuilder:
     1=grass.png
     ```
     """
-    def __init__(self, text: str, router: Router) -> None:
+    def __init__(self, text: str, router: Router, tile_px_size: int) -> None:
         self.text = text
 
         self.lines = self.text.splitlines()
@@ -31,6 +33,9 @@ class MapBuilder:
             key, value = element.split("=")
 
             texture = image.load(self.router.path + value)
+            if texture.get_width() != tile_px_size:
+                self.size = tile_px_size
+                texture = transform.scale_by(texture, tile_px_size / texture.get_width())
 
             self.data.update({key: texture.convert_alpha()})
 
@@ -41,15 +46,12 @@ class MapBuilder:
         y = 0
 
         Pxsize = 1.75
-        size = 50
+        size = self.size
 
         for i in self.lines[1:int(self.height[7:]) + 1]:
             x = 0
             for block in [*i]:
-                if self.data[block] is tuple:
-                    rect(screen, self.data[block] (x*size, y*size, size, size))
-                else:
-                    screen.blit(self.data[block], (x*size, y*size))
+                screen.blit(self.data[block], (x*size, y*size))
                 x += 1
             
             y += 1
